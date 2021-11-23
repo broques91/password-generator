5d8ad08f7972ac2360d258179dcd4da00e849f11
Bastien Roques <bastien.roques971@gmail.com>
2021-11-23 13:58:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const dataLowercase = 'abcdefghijklmnopqrstuvwxyz';
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = '0123456789';
+const dataSymbols = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
+const rangeValue = document.getElementById('password-length');
+const passwordOutput = document.getElementById('password-output');
+
+const generatePassword = () => {
+    let data = [];
+    let password = '';
+
+    console.log(dataUppercase);
+
+    if (lowercase.checked) data.push(...dataLowercase);
+    if (uppercase.checked) data.push(...dataUppercase);
+    if (numbers.checked) data.push(...dataNumbers);
+    if (symbols.checked) data.push(...dataSymbols);
+
+    if (data.length === 0) {
+        alert('Please select at least one item');
+        return;
+    }
+
+    for (let i = 0; i < rangeValue.value; i++) {
+        password += data[Math.floor(Math.random() * data.length)];
+    }
+
+    passwordOutput.value = password;
+    passwordOutput.select();
+    document.execCommand('copy');
+
+    generateButton.textContent = 'Copied !';
+
+    setTimeout(() => {
+        generateButton.textContent = 'Generate';
+    }, 2000);
+
+    // Reset password input value after 1 min
+    setTimeout(() => {
+        passwordOutput.value = 'Password Generator';
+    }, 60 * 1000);
+};
+
+generateButton.addEventListener('click', generatePassword);
